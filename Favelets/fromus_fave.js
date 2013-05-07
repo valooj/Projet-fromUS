5e12215767fd0e32ec38537bb1ab15180ab83d87
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-07 11:01:14 +0200

    Ajout fromus_fave.js
    Test tiré de pony.js afin de faire appel à un code hébergé.

---
+++ Favelets/fromus_fave.js
@@ -0,0 +1,6 @@
+javascript:
+fromus_fave=document.createElement("script");
+fromus_fave.setAttribute('src','https:\/\/docs\.google\.com\/file\/d\/0B9leqFgLTuplRl9yWldXcWJhSE0\/edit?usp=sharing');
+fromus_fave.setAttribute('type','text/javascript');
+fromus_fave.setAttribute('id','fromus_favelet_ID');
+void(document.getElementsByTagName("head")[0].appendChild(fromus_fave));
