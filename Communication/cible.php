715b6688c5ba3fe15e5f12fadf19f3a2fca53091
Nicolas <n_fruteau@hotmail.fr>
2013-05-07 10:55:33 +0200

    ajout de cible.php

---
+++ Communication/cible.php
@@ -0,0 +1,15 @@
+<?php
+ 
+header("Content-Type: text/plain");
+ 
+$store = (isset($_GET["Store"])) ? $_GET["Store"] : NULL;
+$name = (isset($_GET["Name"])) ? $_GET["Name"] : NULL;
+$price = (isset($_GET["Price"])) ? $_GET["Price"] : NULL;
+ 
+if ($nick && $name && $price) {
+    echo "Ok";
+} else {
+    echo "FAILURE";
+}
+ 
+?>
