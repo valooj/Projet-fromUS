9329bb4ee04547ec14912bcbab58800040e5c1b6
Nicolas <n_fruteau@hotmail.fr>
2013-05-07 10:53:10 +0200

    rajout de l'action submit

--- from_us/dialog.js
+++ from_us/dialog.js
@@ -1,3 +1,47 @@
+var xhr = null;
+     
+function getXMLHttpRequest() {
+    if (window.XMLHttpRequest || window.ActiveXObject) {
+        if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch(e) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        } else {
+            xhr = new XMLHttpRequest(); 
+
+        }
+    } else {
+        alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+        return null;
+    }
+    return xhr;
+};
+
+function request() {
+    xhr = getXMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+            alert(xhr.responseText);
+        }
+    };
+    
+    var var_store = encodeURIComponent(localStorage["regStore"]);
+    var var_name = encodeURIComponent(localStorage["regName"]);
+    var var_price = encodeURIComponent(localStorage["regPrice"]);
+
+
+ 	var data ="Store="+var_store+"&Name="+var_name+"&Price="+var_price;
+
+	xhr.open("GET", "http://localhost/projetFU/Communication/cible.php?"+data, true);
+
+ 	xhr.send(null);
+ 	alert("fin send");
+
+};
+
 $(document).ready(function() {
 
 console.log( "Variable from Content Script: "+localStorage["regStore"] );
@@ -29,7 +73,9 @@ if (isOpen != true) {
 		maxWidth: 400,
 		closeOnEscape: false,
 		buttons: [
-			{text: "Submit", click: function() {}},
+			{text: "Submit", click: function() {
+				request();
+			}},
 	
 			// bouton cancel, mettre destroy au lieu de close pour supprimer completement la dialog box
 			{text: "Cancel", click: function() {
