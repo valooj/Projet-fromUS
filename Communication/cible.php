18d3f3493df2f9c0b9dfd16f9a1c395240acd9b3
Nicolas <n_fruteau@hotmail.fr>
2013-05-07 10:59:18 +0200

    rectification

--- Communication/cible.php
+++ Communication/cible.php
@@ -6,7 +6,7 @@ $store = (isset($_GET["Store"])) ? $_GET["Store"] : NULL;
 $name = (isset($_GET["Name"])) ? $_GET["Name"] : NULL;
 $price = (isset($_GET["Price"])) ? $_GET["Price"] : NULL;
  
-if ($nick && $name && $price) {
+if ($store && $name && $price) {
     echo "Ok";
 } else {
     echo "FAILURE";
