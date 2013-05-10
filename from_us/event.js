72377776d146c950fd8896bedfd26fb13fd14955
Nicolas <n_fruteau@hotmail.fr>
2013-05-11 01:26:19 +0200

    changement mineur

--- from_us/event.js
+++ from_us/event.js
@@ -4,8 +4,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 		//chrome.tabs.executeScript(null,{file:'jquery/jquery.min.js',allFrames:false});
 		//chrome.tabs.executeScript(null,{file:'jquery/jquery-ui.js',allFrames:false});
         chrome.tabs.executeScript(null,{file:'fromus_favelet_comments.js',allFrames:false});
-		//chrome.tabs.executeScript(null,{file:'dialog.js',allFrames:false});
-		chrome.tabs.executeScript(null,{file:'passlog.js',allFrames:false});
+		chrome.tabs.executeScript(null,{file:'dialog.js',allFrames:false});
+		//chrome.tabs.executeScript(null,{file:'passlog.js',allFrames:false});
 	});
 
 
