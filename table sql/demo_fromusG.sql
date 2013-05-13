cc7d94ac5423d85d09bbc084927a6faf1716f275
Nicolas <n_fruteau@hotmail.fr>
2013-05-13 11:10:24 +0200

    nouvelle table

--- table sql/demo_fromusG.sql
+++ table sql/demo_fromusG.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Lun 13 Mai 2013 à 08:09
+-- Généré le : Lun 13 Mai 2013 à 09:09
 -- Version du serveur: 5.5.8
 -- Version de PHP: 5.3.5
 
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `produits` (
   `prd_vis` int(1) NOT NULL,
   PRIMARY KEY (`prd_libelle`),
   UNIQUE KEY `key` (`prd_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=58 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=63 ;
 
 --
 -- Contenu de la table `produits`
@@ -75,12 +75,14 @@ CREATE TABLE IF NOT EXISTS `produits` (
 INSERT INTO `produits` (`prd_id`, `prd_libelle`, `prd_site`, `prd_desc`, `prd_cat`, `prd_visu`, `prd_prix`, `prd_vis`) VALUES
 (13, 'Android 4.0 (ICS) MID 9" Capacitive Touch Screen Wifi Webcam G-sensor HDMI 3D, 512MB DDR RAM, 8GB St', 'rakuten.com', 'dscnico', 99, 'visunico', '94.79', 1),
 (12, 'Asics GT 2160 Women''s Running Shoes Sneakers Wide Width T156N-7401 Size 6.5', 'rakuten.com', 'dscnico', 99, 'visunico', '54.99', 1),
-(57, 'Directed 516U Universal Voice Module', 'rakuten.com/prod/directed-516u-universal-voice-module/90148203.html', 'dscnico', 99, 'visunico', '30.10', 1),
+(61, 'Darksiders II (Limited Edition)', 'rakuten.com/prod/darksiders-ii-limited-edition/222393651.html', 'dscnico', 99, 'visunico', '49.30', 1),
+(59, 'Directed 516U Universal Voice Module', 'rakuten.com/prod/directed-516u-universal-voice-module/90148203.html', 'dscnico', 99, 'visunico', '30.10', 1),
 (46, 'Driveway Patrol Infrared Wireless Alert System', 'rakuten.com', 'dscnico', 99, 'visunico', '15.72', 1),
 (11, 'English Laundry Floral Cuff Plaid Mens Dress Shirt Black Size S', 'rakuten.com', 'dscnico', 99, 'visunico', '29.99', 1),
 (54, 'Kensington Desk Mount Cable Anchor', 'rakuten.com', 'dscnico', 99, 'visunico', '13.49', 1),
 (53, 'Kensington Promixo Starter Kit', 'rakuten.com', 'dscnico', 99, 'visunico', '49.99', 1),
 (48, 'Kguard All-in-One Surveillance Combo Kit - 8CH H.264 DVR with 8 CMOS Cameras - 8 x Camera, Digital V', 'rakuten.com', 'dscnico', 99, 'visunico', '517.99', 0),
+(62, 'Samsung UN40EH6000 40" LED HDTV 1080p 120Hz', 'rakuten.com/prod/samsung-un40eh6000-40-led-hdtv-1080p-120hz/228474864.html', 'dscnico', 99, 'visunico', '638.99', 1),
 (51, 'Zmodo 4CH H.264 Video CCTV Real-time Surveillance Security DVR - iPhone & Android Network - No Hard ', 'rakuten.com', 'dscnico', 99, 'visunico', '85.50', 1),
 (42, 'Zmodo 4CH Security Surveillance System for Home with 4 Day Night Weatherproof Bullet Cameras', 'rakuten.com', 'dscnico', 99, 'visunico', '107.99', 1);
 
