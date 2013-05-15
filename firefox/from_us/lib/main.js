657996885b9d26a1f352bfdbb361f0aed13f5c04
sebastien sy <seb6890@gmail.com>
2013-05-15 10:41:07 +0200

    firefox update

--- firefox/from_us/lib/main.js
+++ firefox/from_us/lib/main.js
@@ -1,7 +1,29 @@
 // Import the page-mod API
-var pageMod = require("sdk/page-mod");
+var pageMod = require('sdk/page-mod');
 // Import the self API
-var self = require("sdk/self");
+var self = require('sdk/self');
+var tabs = require('tabs');
+
+var tbb = require('toolbarbutton').ToolbarButton({
+      id: 'from-us_button',
+      label: 'from-us',
+      image: self.data.url('img/on.png'),
+      onCommand: function () {
+        //tbb.destroy(); 
+        tabs.activeTab.attach ({
+        	contentScript:
+        		'var fromus_offre = document.location.href;' +
+        		'window.alert(fromus_offre);'
+		});
+      }
+    });
+
+    if (require('self').loadReason == 'install') {
+      tbb.moveTo({
+        toolbarID: 'nav-bar',
+        forceMove: false // only move from palette
+      });
+    }
 
 // worker = tab.attach(script.js);
 // worker.on("message",handleMessage);
@@ -9,12 +31,12 @@ var self = require("sdk/self");
 // Create a page mod
 // It will run a script whenever a ".org" URL is loaded
 // The script replaces the page contents with a message
-pageMod.PageMod({
-  include: "http://*",
-  contentScriptFile: [self.data.url("jquery.min.js"),
-                      self.data.url("fromus_favelet_comments.js")],
-  contentScriptWhen: "end"
-});
+/*pageMod.PageMod({
+  include: 'http://*',
+  contentScriptFile: [self.data.url('jquery.min.js'),
+                      self.data.url('fromus_favelet_comments.js')],
+  contentScriptWhen: 'end'
+});*/
 
 // handleMessage(message);
 // request.post(message);
