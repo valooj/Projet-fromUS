61d3f054a960d2e19eca100a9c9070817a215089
Valériane <jean.valeriane@gmail.com>
2013-05-16 10:07:55 +0200

    update download.php

--- Communication/download.php
+++ Communication/download.php
@@ -1,7 +1,7 @@
 <?php
 	include_once 'HZip.class.php';
 	include_once 'Functions.class.php';
-	//$id_user = $_SESSION['usr_fus']['id'];
+	$id_user ='54fv4sdrtgd455yhbd4';
 
 	//Delete folder function 
 	
