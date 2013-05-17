9f80146751cbe2528d4c9400590027a6b780cd8a
sebastien sy <seb6890@gmail.com>
2013-05-17 16:19:38 +0200

    commit

--- from_us/dialog.js
+++ from_us/dialog.js
@@ -84,256 +84,19 @@ function sendAjoutPanier() {
 
 $(document).ready(function() {
 
-<<<<<<< HEAD
 	console.log( "Variable from Content Script: "+localStorage["regStore"] );
 	console.log( "Variable from Content Script: "+localStorage["regName"] );
 	console.log( "Variable from Content Script: "+localStorage["regPrice"] );
 	console.log( "Variable from Content Script: "+localStorage["regDesc"] );
 
 	// creation de la dialog box
-	//var newDialog = $('<div id="dialogBox"><input type="button" value="login" id="log" /> <p>Formulaire</p><form id="myForm"><label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br><label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br><label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br><label for="category">Catégorie:</label><select id="category"><option value="default" selected="selected">Choisir une catégorie</option><option value="1">Antiques, Art & Collectibles</option><option value="24">Auto & Moto</option><option value="15">Bijoux & Montres</option><option value="16">Chaussures</option><option value="13">Entreprises & Industries</option><option value="19">Habits pour enfants</option><option value="26">Habits pour femmes</option><option value="25">Habits pour hommes</option><option value="14">Instrument de musique</option><option value="17">Jeux & Jouets</option><option value="28">Jeux vidéos & Informatique</option><option value="3">Livres, Films & Musiques</option><option value="30">Maison & Jardin</option><option value="27">Ordinateurs & Bureau</option><option value="23">Pièces, Bricolage & Outillage</option><option value="18">Puericulture</option><option value="21">Sacs & Accessoires</option><option value="29">Santé & Beauté</option><option value="12">Sports & Loisirs</option></select></br><label for="quantite">Quantite : </label><input id="QteSpinner"></form></div>');
+	var newDialog = $('<div id="dialogBox"><input type="button" value="login" id="log" /> <p>Formulaire</p><form id="myForm"><label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br><label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br><label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br><label for="category">Catégorie:</label><select id="category"><option value="default" selected="selected">Choisir une catégorie</option><option value="1">Antiques, Art & Collectibles</option><option value="24">Auto & Moto</option><option value="15">Bijoux & Montres</option><option value="16">Chaussures</option><option value="13">Entreprises & Industries</option><option value="19">Habits pour enfants</option><option value="26">Habits pour femmes</option><option value="25">Habits pour hommes</option><option value="14">Instrument de musique</option><option value="17">Jeux & Jouets</option><option value="28">Jeux vidéos & Informatique</option><option value="3">Livres, Films & Musiques</option><option value="30">Maison & Jardin</option><option value="27">Ordinateurs & Bureau</option><option value="23">Pièces, Bricolage & Outillage</option><option value="18">Puericulture</option><option value="21">Sacs & Accessoires</option><option value="29">Santé & Beauté</option><option value="12">Sports & Loisirs</option></select></br><label for="quantite">Quantite : </label><input id="QteSpinner"></form></div>');
 
 	// variable qui permet de savoir si la dialog box est ouverte
-	//var isOpen = $("#dialogBox").dialog("isOpen");
-
-	// variable qui permet de savoir si la dialog box est ouverte
-	var test = document.getElementById('fromus_form');
-
-	if (test == null) {
-		// creation de la popup
-		$.Zebra_Dialog({
-		    'type': 'false',
-			'modal': false,
-			'width': 450,
-			'position': ['left + 20', 'top + 20'],
-			'overlay_close': false,
-			'overlay_opacity': 1,
-			'auto_close': false,
-			'title': 'From-us.com',
-<<<<<<< HEAD
-			'onClose': function(caption) {
-				//
-=======
-console.log( "Variable from Content Script: "+localStorage["regStore"] );
-console.log( "Variable from Content Script: "+localStorage["regName"] );
-console.log( "Variable from Content Script: "+localStorage["regPrice"] );
-console.log( "Variable from Content Script: "+localStorage["regDesc"] );
-
-// creation de la dialog box
-//var newDialog = $('<div id="dialogBox"><input type="button" value="login" id="log" /> <p>Formulaire</p><form id="myForm"><label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br><label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br><label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br><label for="category">Catégorie:</label><select id="category"><option value="default" selected="selected">Choisir une catégorie</option><option value="1">Antiques, Art & Collectibles</option><option value="24">Auto & Moto</option><option value="15">Bijoux & Montres</option><option value="16">Chaussures</option><option value="13">Entreprises & Industries</option><option value="19">Habits pour enfants</option><option value="26">Habits pour femmes</option><option value="25">Habits pour hommes</option><option value="14">Instrument de musique</option><option value="17">Jeux & Jouets</option><option value="28">Jeux vidéos & Informatique</option><option value="3">Livres, Films & Musiques</option><option value="30">Maison & Jardin</option><option value="27">Ordinateurs & Bureau</option><option value="23">Pièces, Bricolage & Outillage</option><option value="18">Puericulture</option><option value="21">Sacs & Accessoires</option><option value="29">Santé & Beauté</option><option value="12">Sports & Loisirs</option></select></br><label for="quantite">Quantite : </label><input id="QteSpinner"></form></div>');
-
-// variable qui permet de savoir si la dialog box est ouverte
-//var isOpen = $("#dialogBox").dialog("isOpen");
-
-
-// creation de la popup
-$.Zebra_Dialog({
-    'type': 'false',
-	'modal': false,
-	'width': 450,
-	'position': ['left + 20', 'top + 20'],
-	'title': 'From-us.com',
-	'message': '<input type="button" value="Login" id="log" /></br>' + 
-				'<form id="myForm">' +
-					'<label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br>' +
-					'<label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br>' +
-					'<label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br>' + 
-					'<label for="category">Catégorie:</label>' +
-						'<select id="category">' +
-							'<option value="default" selected="selected">Choisir une catégorie</option>' +
-							'<option value="1">Antiques, Art & Collectibles</option>' +
-							'<option value="24">Auto & Moto</option>' +
-							'<option value="15">Bijoux & Montres</option>' + 
-							'<option value="16">Chaussures</option>' + 
-							'<option value="13">Entreprises & Industries</option>' + 
-							'<option value="19">Habits pour enfants</option>' + 
-							'<option value="26">Habits pour femmes</option>' +
-							'<option value="25">Habits pour hommes</option>' +
-							'<option value="14">Instrument de musique</option>' + 
-							'<option value="17">Jeux & Jouets</option>' + 
-							'<option value="28">Jeux vidéos & Informatique</option>' + 
-							'<option value="3">Livres, Films & Musiques</option>' +
-							'<option value="30">Maison & Jardin</option>' + 
-							'<option value="27">Ordinateurs & Bureau</option>' + 
-							'<option value="23">Pièces, Bricolage & Outillage</option>' + 
-							'<option value="18">Puericulture</option>' +
-							'<option value="21">Sacs & Accessoires</option>' +
-							'<option value="29">Santé & Beauté</option>' + 
-							'<option value="12">Sports & Loisirs</option>' +
-						'</select></br>' +
-					'<label for="quantite">Quantite : </label><input id="QteSpinner">' + 
-				'</form>',
-    'buttons':  [
-					// bouton commander
-                    {caption: 'Commander', callback: function() { 
-						var qteSpinner = document.getElementById("QteSpinner");
-						qteVal = qteSpinner.value;
-						var categSelect = document.getElementById("category");
-						categVal = categSelect.value;
-
-						var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-						var postData = JSON.stringify(jsonProduct);
-						productJSON = {product:postData};
-						sendToServer(_urlProduct,productJSON);
-
-
-						var jsonCalcul = {libelle: regName, qte: qteVal ,montant: regPrice ,categ: categVal};
-						var postDataCalcul = JSON.stringify(jsonCalcul);
-						calculJSON = {calcul:postDataCalcul};
-						sendToServer(_urlCalcul , calculJSON);
-					}},
-						
-					// bouton soummettre un produit qui permet d'ajouter un produit dans la base de données
-                    {caption: 'Ajouter fiche produit', callback: function() { 
-						var qteSpinner = document.getElementById("QteSpinner");
-						qteVal = qteSpinner.value;
-						var categSelect = document.getElementById("category");
-						categVal = categSelect.value;
-
-						var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-						var postData = JSON.stringify(jsonProduct);
-						productJSON = {product:postData};
-						sendToServer(_urlProduct , productJSON);
-					}},
-					
-					// bouton annuler
-                    {caption: 'Annuler', callback: function() { close()}}
-                ]
-});
-
-// permet de deplacer la popup
-$( ".ZebraDialog" ).draggable();
-
-// si il n'y a pas de la dialog box alors on l'a crée
-/*if (isOpen != true) {
-	
-	newDialog.dialog({
-		modal: false,
-		title: "From-us.com",
-		show: 'clip',
-		hide: 'clip',
-		autoOpen: false,
-		position: {
-			my: "left top", 
-			at: "left top"
-		},
-
-		width: 400,
-		height: 300,
-		maxHeight: 300,
-		maxWidth: 400,
-		closeOnEscape: false,
-		buttons: [
-			// bouton submit qui permet de commander un produit (ne marche pas pour l'instant)
-			{
-				text: "Submit", 
-				title: "Permet de commander", 
-				click: function() {
-
-					var qteSpinner = document.getElementById("QteSpinner");
-					qteVal = qteSpinner.value;
-					var categSelect = document.getElementById("category");
-					categVal = categSelect.value;
-
-					var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-					var postData = JSON.stringify(jsonProduct);
-					productJSON = {product:postData};
-					sendToServer(_urlProduct,productJSON);
-
-
-					var jsonCalcul = {libelle: regName, qte: qteVal ,montant: regPrice ,categ: categVal};
-					var postDataCalcul = JSON.stringify(jsonCalcul);
-					calculJSON = {calcul:postDataCalcul};
-					sendToServer(_urlCalcul , calculJSON);
-				}
-			},
-	
-			// bouton cancel, mettre destroy au lieu de close pour supprimer completement la dialog box
-			{
-				text: "Cancel", 
-				title: "Fermer la pop up",
-				click: function() {
-					$(this).dialog('destroy');
-				}
->>>>>>> authen
-			},
-=======
-			'onClose': false,
->>>>>>> commit
-			'message': '<input type="button" value="Login" id="log" /></br>' + 
-						'<form id="fromus_form">' +
-							'<label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br>' +
-							'<label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br>' +
-							'<label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br>' + 
-							'<label for="category">Catégorie:</label>' +
-								'<select id="category">' +
-									'<option value="default" selected="selected">Choisir une catégorie</option>' +
-									'<option value="1">Antiques, Art & Collectibles</option>' +
-									'<option value="24">Auto & Moto</option>' +
-									'<option value="15">Bijoux & Montres</option>' + 
-									'<option value="16">Chaussures</option>' + 
-									'<option value="13">Entreprises & Industries</option>' + 
-									'<option value="19">Habits pour enfants</option>' + 
-									'<option value="26">Habits pour femmes</option>' +
-									'<option value="25">Habits pour hommes</option>' +
-									'<option value="14">Instrument de musique</option>' + 
-									'<option value="17">Jeux & Jouets</option>' + 
-									'<option value="28">Jeux vidéos & Informatique</option>' + 
-									'<option value="3">Livres, Films & Musiques</option>' +
-									'<option value="30">Maison & Jardin</option>' + 
-									'<option value="27">Ordinateurs & Bureau</option>' + 
-									'<option value="23">Pièces, Bricolage & Outillage</option>' + 
-									'<option value="18">Puericulture</option>' +
-									'<option value="21">Sacs & Accessoires</option>' +
-									'<option value="29">Santé & Beauté</option>' + 
-									'<option value="12">Sports & Loisirs</option>' +
-								'</select></br>' +
-							'<label for="quantite">Quantite : </label><input id="QteSpinner">' + 
-						'</form>',
-		    'buttons':  [
-							// bouton commander
-		                    {caption: 'Commander', callback: function() { 
-								var qteSpinner = document.getElementById("QteSpinner");
-								qteVal = qteSpinner.value;
-								var categSelect = document.getElementById("category");
-								categVal = categSelect.value;
-
-								var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-								var postData = JSON.stringify(jsonProduct);
-								productJSON = {product:postData};
-								sendToServer(_urlProduct,productJSON);
-
-
-								var jsonCalcul = {libelle: regName, qte: qteVal ,montant: regPrice ,categ: categVal};
-								var postDataCalcul = JSON.stringify(jsonCalcul);
-								calculJSON = {calcul:postDataCalcul};
-								sendToServer(_urlCalcul , calculJSON);
-							}},
-								
-							// bouton soummettre un produit qui permet d'ajouter un produit dans la base de données
-		                    {caption: 'Ajouter fiche produit', callback: function() { 
-								var qteSpinner = document.getElementById("QteSpinner");
-								qteVal = qteSpinner.value;
-								var categSelect = document.getElementById("category");
-								categVal = categSelect.value;
-
-								var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-								var postData = JSON.stringify(jsonProduct);
-								productJSON = {product:postData};
-								sendToServer(_urlProduct , productJSON);
-							}},
-							
-							// bouton annuler
-		                    {caption: 'Annuler', callback: function() { 
-		                    	}}
-		                ]
-		});
-
-		// permet de deplacer la popup
-		$( ".ZebraDialog" ).draggable();
+	var isOpen = $("#dialogBox").dialog("isOpen");
 
 		// si il n'y a pas de la dialog box alors on l'a crée
-		/*if (isOpen != true) {
+		if (isOpen != true) {
 			
 			newDialog.dialog({
 				modal: false,
@@ -359,20 +122,20 @@ $( ".ZebraDialog" ).draggable();
 						click: function() {
 
 							var qteSpinner = document.getElementById("QteSpinner");
-							qteVal = qteSpinner.value;
-							var categSelect = document.getElementById("category");
-							categVal = categSelect.value;
+						qteVal = qteSpinner.value;
+						var categSelect = document.getElementById("category");
+						categVal = categSelect.value;
 
-							var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-							var postData = JSON.stringify(jsonProduct);
-							productJSON = {product:postData};
-							sendToServer(_urlProduct,productJSON);
+						var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
+						var postData = JSON.stringify(jsonProduct);
+						productJSON = {product:postData};
+						sendToServer(_urlProduct,productJSON);
 
 
-							var jsonCalcul = {libelle: regName, qte: qteVal ,montant: regPrice ,categ: categVal};
-							var postDataCalcul = JSON.stringify(jsonCalcul);
-							calculJSON = {calcul:postDataCalcul};
-							sendToServer(_urlCalcul , calculJSON);
+						var jsonCalcul = {libelle: regName, qte: qteVal ,montant: regPrice ,categ: categVal};
+						var postDataCalcul = JSON.stringify(jsonCalcul);
+						calculJSON = {calcul:postDataCalcul};
+						sendToServer(_urlCalcul , calculJSON);
 						}
 					},
 			
@@ -393,21 +156,21 @@ $( ".ZebraDialog" ).draggable();
 						click: function() {
 
 							var qteSpinner = document.getElementById("QteSpinner");
-							qteVal = qteSpinner.value;
-							var categSelect = document.getElementById("category");
-							categVal = categSelect.value;
+						qteVal = qteSpinner.value;
+						var categSelect = document.getElementById("category");
+						categVal = categSelect.value;
 
-							var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
-							var postData = JSON.stringify(jsonProduct);
-							productJSON = {product:postData};
-							sendToServer(_urlProduct , productJSON);
+						var jsonProduct = {prd_libelle: regName ,prd_site: regOffer, prd_desc: regDesc, prd_visu: regVisu, prd_prix: regPrice, prd_cat: categVal};
+						var postData = JSON.stringify(jsonProduct);
+						productJSON = {product:postData};
+						sendToServer(_urlProduct , productJSON);
 						}
 					},
 						
 				
 				
 				]
-			});*/
+			});
 			
 			
 			// creation du spinner pour la quantite
@@ -453,7 +216,7 @@ $( ".ZebraDialog" ).draggable();
 			}, false);
 
 			// ouverture de la dialog box
-			//newDialog.dialog("open");
+			newDialog.dialog("open");
 			connect(_urlConnect);
 			
 			// suppression des key dans le localstorage
