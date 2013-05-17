f42a745aeff05e1aced4fd8bb212e65f20744abf
sebastien sy <seb6890@gmail.com>
2013-05-17 17:16:02 +0200

    création d'onglet dans la pop up (en cours)
    pas encore fini et fonctionnel

--- from_us/dialog.js
+++ from_us/dialog.js
@@ -90,15 +90,34 @@ $(document).ready(function() {
 	console.log( "Variable from Content Script: "+localStorage["regDesc"] );
 
 	// creation de la dialog box
-	var newDialog = $('<div id="dialogBox"><input type="button" value="login" id="log" /> <p>Formulaire</p><form id="myForm"><label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br><label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br><label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br><label for="category">Catégorie:</label><select id="category"><option value="default" selected="selected">Choisir une catégorie</option><option value="1">Antiques, Art & Collectibles</option><option value="24">Auto & Moto</option><option value="15">Bijoux & Montres</option><option value="16">Chaussures</option><option value="13">Entreprises & Industries</option><option value="19">Habits pour enfants</option><option value="26">Habits pour femmes</option><option value="25">Habits pour hommes</option><option value="14">Instrument de musique</option><option value="17">Jeux & Jouets</option><option value="28">Jeux vidéos & Informatique</option><option value="3">Livres, Films & Musiques</option><option value="30">Maison & Jardin</option><option value="27">Ordinateurs & Bureau</option><option value="23">Pièces, Bricolage & Outillage</option><option value="18">Puericulture</option><option value="21">Sacs & Accessoires</option><option value="29">Santé & Beauté</option><option value="12">Sports & Loisirs</option></select></br><label for="quantite">Quantite : </label><input id="QteSpinner"></form></div>');
+	//var newDialog = $('<div id="dialogBox"><input type="button" value="login" id="log" /> <p>Formulaire</p><form id="myForm"><label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br><label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br><label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br><label for="category">Catégorie:</label><select id="category"><option value="default" selected="selected">Choisir une catégorie</option><option value="1">Antiques, Art & Collectibles</option><option value="24">Auto & Moto</option><option value="15">Bijoux & Montres</option><option value="16">Chaussures</option><option value="13">Entreprises & Industries</option><option value="19">Habits pour enfants</option><option value="26">Habits pour femmes</option><option value="25">Habits pour hommes</option><option value="14">Instrument de musique</option><option value="17">Jeux & Jouets</option><option value="28">Jeux vidéos & Informatique</option><option value="3">Livres, Films & Musiques</option><option value="30">Maison & Jardin</option><option value="27">Ordinateurs & Bureau</option><option value="23">Pièces, Bricolage & Outillage</option><option value="18">Puericulture</option><option value="21">Sacs & Accessoires</option><option value="29">Santé & Beauté</option><option value="12">Sports & Loisirs</option></select></br><label for="quantite">Quantite : </label><input id="QteSpinner"></form></div>');
+
+	var newDialog = $('<div id="dialogBox">' +
+						'<div id="tabs">' +
+							'<ul>' +
+								'<li><a href="#tabs-1">tab 1</a></li>' +
+								'<li><a href="#tabs-2">tab 2</a></li>' +
+								'<li><a href="#tabs-3">tab 3</a></li>' +
+							'</ul>' +
+							'<div id="tabs-1"><p>Formulaire</p><form id="myForm"><label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br><label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br><label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br><label for="category">Catégorie:</label><select id="category"><option value="default" selected="selected">Choisir une catégorie</option><option value="1">Antiques, Art & Collectibles</option><option value="24">Auto & Moto</option><option value="15">Bijoux & Montres</option><option value="16">Chaussures</option><option value="13">Entreprises & Industries</option><option value="19">Habits pour enfants</option><option value="26">Habits pour femmes</option><option value="25">Habits pour hommes</option><option value="14">Instrument de musique</option><option value="17">Jeux & Jouets</option><option value="28">Jeux vidéos & Informatique</option><option value="3">Livres, Films & Musiques</option><option value="30">Maison & Jardin</option><option value="27">Ordinateurs & Bureau</option><option value="23">Pièces, Bricolage & Outillage</option><option value="18">Puericulture</option><option value="21">Sacs & Accessoires</option><option value="29">Santé & Beauté</option><option value="12">Sports & Loisirs</option></select></br><label for="quantite">Quantite : </label><input id="QteSpinner"></form></div>' +
+							'<div id="tabs-2">test 2</div>' +
+							'<div id="tabs-3">test 3</div>' +
+						'</div>' +
+					'</div>');
 
 	// variable qui permet de savoir si la dialog box est ouverte
-	var isOpen = $("#dialogBox").dialog("isOpen");
+	//var isOpen = $("#dialogBox").dialog("isOpen");
+
+		//newDialog.tabs();
+
+		newDialog.tabs();
+    newDialog.dialog();
+    
 
 		// si il n'y a pas de la dialog box alors on l'a crée
-		if (isOpen != true) {
+		//if (isOpen != true) {
 			
-			newDialog.dialog({
+			/*newDialog.dialog({
 				modal: false,
 				title: "From-us.com",
 				show: 'clip',
@@ -170,7 +189,7 @@ $(document).ready(function() {
 				
 				
 				]
-			});
+			});*/
 			
 			
 			// creation du spinner pour la quantite
@@ -225,8 +244,8 @@ $(document).ready(function() {
 			localStorage.removeItem('regPrice');
 			localStorage.removeItem('regStore');
 			localStorage.removeItem('regOffer');
-
-	}
+		
+	//}
 });
 
 
