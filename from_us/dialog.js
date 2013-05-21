1d0b18d0abce7ab8ff040ed08b0323acc51d5191
sebastien sy <seb6890@gmail.com>
2013-05-21 15:42:02 +0200

    ajout de ajouter et commander

--- from_us/dialog.js
+++ from_us/dialog.js
@@ -112,12 +112,13 @@ $(document).ready(function() {
 	var newDialog = $('<div id="dialogBox">' +
 						'<div id="tabs">' +
 							'<ul>' +
-								'<li><a href="#tabs-1">Commander/Ajouter</a></li>' +
-								'<li><a href="#tabs-2">Mon compte</a></li>' +
+								'<li><a href="#tabs-1">Ajouter</a></li>' +
+								'<li><a href="#tabs-2">Commander</a></li>' +
+								'<li><a href="#tabs-3">Mon compte</a></li>' +
 							'</ul>' +
 							'<div id="tabs-1">' +
-								'<h2>Formulaire</h2>' +
-								'<form id="myForm">' + 
+								'<h2>Formulaire</h2>' + '<input type="button" name="btnfromus" id="btnfromus" value=""><br>' +
+								'<form id="addForm">' + 
 									'<label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br>' +
 									'<label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br>' + 
 									'<label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br>' +
@@ -148,6 +149,38 @@ $(document).ready(function() {
 								'</form>' +
 							'</div>' +
 							'<div id="tabs-2">' +
+								'<h2>Formulaire</h2>' + '<input type="button" name="btn1fromus" id="btn1fromus" value=""><br>' +
+								'<form id="submitForm">' + 
+									'<label for="store">Marchand : </label><input type="textbox" id="store" disabled="true"/></br>' +
+									'<label for="name">Nom du produit : </label><input type="textbox" id="name" disabled="true"/></br>' + 
+									'<label for="price">Prix du produit : </label><input type="textbox" id="price" disabled="true"/></br>' +
+									'<label for="category">Catégorie:</label>' +
+										'<select id="category">' +
+											'<option value="default" selected="selected">Choisir une catégorie</option>' +
+											'<option value="1">Antiques, Art & Collectibles</option>' +
+											'<option value="24">Auto & Moto</option>' +
+											'<option value="15">Bijoux & Montres</option>' +
+											'<option value="16">Chaussures</option>' +
+											'<option value="13">Entreprises & Industries</option>' +
+											'<option value="19">Habits pour enfants</option>' +
+											'<option value="26">Habits pour femmes</option>' +
+											'<option value="25">Habits pour hommes</option>' +
+											'<option value="14">Instrument de musique</option>' +
+											'<option value="17">Jeux & Jouets</option>' +
+											'<option value="28">Jeux vidéos & Informatique</option>' +
+											'<option value="3">Livres, Films & Musiques</option>' +
+											'<option value="30">Maison & Jardin</option>' +
+											'<option value="27">Ordinateurs & Bureau</option>' +
+											'<option value="23">Pièces, Bricolage & Outillage</option>' +
+											'<option value="18">Puericulture</option>' +
+											'<option value="21">Sacs & Accessoires</option>' +
+											'<option value="29">Santé & Beauté</option>' +
+											'<option value="12">Sports & Loisirs</option>' +
+										'</select></br>' +
+									'<label for="quantite">Quantite : </label><input id="QteSpinner">' +
+								'</form>' +
+							'</div>' +
+							'<div id="tabs-3">' +
 								'<h2>From-us.com</h2>' +
 								'<p>Merci d\'entrer votre identifiant et votre mot de passe From-us.com.</p>' +
 								'<label for="idfromus">Identifiant : </label><input type="textbox" id="idfromus" /></br>' +
@@ -179,8 +212,8 @@ $(document).ready(function() {
 					at: "left top"
 				},
 			height: 300,
-			width: 850,
-			resizable: false,
+			width: 855,
+			resizable: true,
 			closeOnEscape: true,
 
 			// bouton X => ferme la pop up
@@ -318,6 +351,16 @@ $(document).ready(function() {
 			}
 				    
 		}, false);
+
+		var btnfromus = document.getElementById('btnfromus');
+		btnfromus.addEventListener('click', function(e) {
+			window.open('http://www.from-us.com/fromus/fr/index.html');
+		},false);
+
+		var btn1fromus = document.getElementById('btn1fromus');
+		btn1fromus.addEventListener('click', function(e) {
+			window.open('http://www.from-us.com/fromus/fr/index.html');
+		},false);
 
 		// ouverture de la dialog box
 		newDialog.dialog("open");
