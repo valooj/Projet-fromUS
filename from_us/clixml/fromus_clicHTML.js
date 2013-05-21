202d80cd9bef8dedb6363efc8bcd969b8328d27a
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-21 11:05:28 +0200

    Creation de fromus_clicHTML.js
    À terme, permettra de modifier la couleur du background de l'élément
    survolé et de récupérer l'innerHTML de l'élément cliqué.
    Pour le moment, ne fait que récuperer l'innerHTML.

---
+++ from_us/clixml/fromus_clicHTML.js
@@ -0,0 +1,41 @@
+var c=1;
+var b[];
+var bindEvent = function(elem ,evt,cb) {
+	//vérifie si addEventListenerexiste dans l'élément
+	if ( elem.addEventListener ) {
+		elem.addEventListener(evt,cb,false);
+        //si addEventListener n'est pas présent, vérifie si le navigateur est une version  d'IE
+        } else if ( elem.attachEvent ) {
+		//ajoute le préfixe "on" à l'event
+		elem.attachEvent('on' + evt, function(){
+			// Simule addEventListener ; s'assure que le callback obtient l'élément pour "this" et s'assure que le premier élément de la fonction est un event
+			cb.call(event.srcElement,event);
+		});
+	}
+}
+
+
+bindEvent(document,'mouseOver', function(event) 
+{ var target = event.target || event.srcElement;
+			if (!c) return;
+			c = 0;
+			t = target;
+			b[t] = t.style.backgroundColor;
+			t.style.background = '#DADADA';
+	});
+
+bindEvent(document,'mouseOut', function(event) 
+{ var target = event.target || event.srcElement;
+			t = target;
+			t.style.backgroundColor = b[t];
+			c = 1;	
+});
+
+bindEvent(document,'click', function(event) 
+{ var target = event.target || event.srcElement;
+	alert ( target.innerHTML );
+		this.removeEventListener('mouseOut',arguments.callee,false);
+					t.style.backgroundColor = b[t];
+		this.removeEventListener('mouseOver',arguments.callee,false);
+	this.removeEventListener('click',arguments.callee,false);
+});
