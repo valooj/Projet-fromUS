826b78376d5d918ea8f873ec600ca8f4ef579661
Nicolas <n_fruteau@hotmail.fr>
2013-05-22 17:04:05 +0200

    categorie marche ainsi que le json

--- Communication/cible3.php
+++ Communication/cible3.php
@@ -514,11 +514,11 @@ try
 			    '_lang' => $get_language));
 			$arrs = $reqs->fetch();
 			while($arrs = $reqs->fetch()){
-				$scategorie = $scategorie.',{id:'.$arrs[0].',libelle:'.$arrs[1].'}';
+				$scategorie = $scategorie.',{"idCat":"'.$arrs[0].'","libelleCat":"'.$arrs[1].'"}';
 			}
 			
 			//$categorie = $categorie.' {'.$arr[0].' : '.$arr[1].' '.$scategorie.'} ';
-			$categorie = $categorie.',{"idCat":"'.$arr[0].'","libelleCat":"'.$arr[1].'"}';   //.$scategorie;
+			$categorie = $categorie.',{"idCat":"'.$arr[0].'","libelleCat":"'.$arr[1].'"}'.$scategorie;
 			$scategorie= null;
 			}
 			
