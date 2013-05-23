07843aa767f3676a949df7332b5ad916feab8dd5
sebastien sy <seb6890@gmail.com>
2013-05-23 11:55:53 +0200

    update de l'extension
    intégration de fromus_recuperation.js

--- from_us/event.js
+++ from_us/event.js
@@ -12,7 +12,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
 chrome.browserAction.onClicked.addListener(function(tab) {
 		//chrome.tabs.executeScript(null,{file:'jquery/jquery.min.js',allFrames:false});
 		//chrome.tabs.executeScript(null,{file:'jquery/jquery-ui.js',allFrames:false});
-        chrome.tabs.executeScript(null,{file:'fromus_favelet_comments.js',allFrames:false});
+        chrome.tabs.executeScript(null,{file:'/clixml/fromus_recuperation.js',allFrames:false});
 		chrome.tabs.executeScript(null,{file:'dialog.js',allFrames:false});
 		//chrome.tabs.executeScript(null,{file:'fromus_onglets.js',allFrames:false});
 		//chrome.tabs.executeScript(null,{file:'passlog.js',allFrames:false});
