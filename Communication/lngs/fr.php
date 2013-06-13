753dd58999b8062dd6fb29a1ba863abc56802edc
Valériane <jean.valeriane@gmail.com>
2013-06-13 16:18:03 +0200

    changement css

--- Communication/lngs/fr.php
+++ Communication/lngs/fr.php
@@ -1,27 +1,27 @@
 <?php
 
-	$lng['invalid_token'] = 'Une erreur est survenue: Reconnectez-vous !';
-	$lng['invalid_product'] = 'Une erreur est survenue: le produit est invalide';
-	$lng['bad_param'] ='Une erreur est survenue: votre panier contient de mauvais paramètres ';
-	$lng['invalid_price'] = 'Une erreur est survenue : le prix est invalide';
-	$lng['invalid_insert'] = 'Une erreur est survenue : l’insertion a échoué';
+	$lng['invalid_token'] = 'Attention ! Reconnectez-vous !';
+	$lng['invalid_product'] = 'Attention ! le produit est invalide';
+	$lng['bad_param'] ='Attention ! votre panier contient de mauvais paramètres ';
+	$lng['invalid_price'] = 'Attention ! le prix est invalide';
+	$lng['invalid_insert'] = 'Attention ! l’insertion a échoué';
 	$lng['win'] = 'Vous avez gagné XX points !';
-	$lng['invalid_panier'] ='Une erreur est survenue : le prix est invalide';
-	$lng['invalid_qte'] = 'Une erreur est survenue : mauvais paramètre de quantité';
+	$lng['invalid_panier'] ='Attention ! le prix est invalide';
+	$lng['invalid_qte'] = 'Attention ! mauvais paramètre de quantité';
 
-	$lng['product_insert'] = 'Votre commande est en cours de validation par nos services.';
-	$lng ['invalid_calcul'] = 'Une erreur est survenue Calcul non spécifié';
-	$long['invalid_access'] = 'Une erreur est survenue Access non spécifié';
-	$lng['invalid_country'] = 'Une erreur est survenue Pays invalide';
-	$lng['invalid_log'] ='Une erreur est survenue: vérifier votre Email et votre mot de passe';
-	$lng['invalid_email'] = 'Une erreur est survenue: vérifier votre Email';
+	$lng['product_insert'] = 'Votre commande est en cours de validation.';
+	$lng ['invalid_calcul'] = 'Attention ! Calcul non spécifié';
+	$long['invalid_access'] = 'Attention ! Access non spécifié';
+	$lng['invalid_country'] = 'Attention ! Pays invalide';
+	$lng['invalid_log'] ='Attention ! vérifier votre Email et votre mot de passe';
+	$lng['invalid_email'] = 'Attention ! vérifier votre Email';
 	$lng['logout'] = 'Au revoir';
 	$lng['invalid_categ'] = 'Vous devez choisir une catégorie';
-	$lng['invalid_action'] = ' Une erreur est survenue Action non spécifié';
-	$lng['invalid_bd'] = ' Une erreur est survenue : la connexion au serveur est impossible';
+	$lng['invalid_action'] = ' Attention ! Action non spécifié';
+	$lng['invalid_bd'] = ' Attention ! la connexion au serveur est impossible';
 	$lng['choix_categ'] = ' Choisissez une catégorie ';
 	$lng['choix_scateg'] = ' Choisissez une sous-catégorie';
 
-	$lng['invalid_url'] = 'ce site n’est pas connu dans notre répertoire. Merci de l’ajouter';
+	$lng['invalid_url'] = 'ce site n’est pas connu dans notre répertoire.';
 	$lng['participation'] = 'Merci de nous avoir aidé à amélioré notre site ! ';
 ?>
